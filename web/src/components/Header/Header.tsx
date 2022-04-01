af06922435846cc583322bcdb8e99e7be65c949b
Daniel Choudhury <dannychoudhury@gmail.com>
2022-04-01 14:13:18 +0700

    Use button instead of div and play with styles to center

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -11,10 +11,12 @@ const Header = () => {
   return (
     <div className="p-0 bg-bottom bg-cover bg-forest">
       <div className="bg-gradient-to-b from-transparent via-[#00000044] to-black bg-opacity-10 m-0 p-4 flex-y items-center justify-center">
-        <div
-          className="w-16 h-16 p-0 mx-auto mt-6 transition ease-in-out bg-cover bg-icon hover:-translate-y-1 hover:scale-110"
+        <button
+          className="flex w-16 h-16 p-0 mx-auto mt-6 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-2xl"
           onClick={toggleConfetti}
-        />
+        >
+          <div className="w-full h-full bg-cover bg-icon" />
+        </button>
         <div className="mb-24 text-center">
           <h1 className="text-5xl font-bold md:text-6xl">
             RedwoodJS
